23c4816be227b450344d41541d6655927ed0458d
Ariele Matthes <ariele0426@gmail.com>
2023-03-04 13:54:30 -0500

    morgage definitions added

--- lesson_2/morgage/morgage_calculator.rb
+++ lesson_2/morgage/morgage_calculator.rb
@@ -1,21 +1,55 @@
-puts "Please enter the total loan amount in USD:"
+require 'yaml'
+MESSAGES = YAML.load_file('morgage_messages.yml')
+
+def messages(message)
+  MESSAGES[message]
+end
+
+def prompt(key)
+  message = messages(key)
+  puts("=> #{message}")
+end
+
+def number?(num)
+  /\d/.match(num) && /^-?\d*\.?\d*$/.match(num)
+end
+
+def months(years, months)
+  (years * 12) + months
+end
+
+def monthly_payment(p, j, n)
+  m = p * (j / (1 - (1 + j)**(-n)))
+end
+
+prompt('welcome')
+
+loop do
+prompt('amount')
 loan_amount = gets.chomp.to_i
 
-puts "Please enter the loan term in full years:"
+prompt('years')
 term_years = gets.chomp.to_i
 
-puts "Please enter the loan term in months:"
+prompt('months')
 term_months = gets.chomp.to_i
 
-puts "Please enter the APR on the loan:"
+prompt('apr')
 apr = gets.chomp.to_f
 
 puts "Please confirm your loan is for $#{loan_amount} for a term of #{term_years} years and #{term_months} months with an APR of #{apr}% (y/n)"
 confirmation = gets.chomp 
 
-term = (term_years * 12) + term_months
+term = months(term_years, term_months)
 
-
-monthly_rate = apr/1200
-
+monthly_rate =  case apr
+                when < 0
+                  apr
+                when > 0
+                  apr/100
+                end
 p monthly_payment = loan_amount * (monthly_rate/(1 - (1 + monthly_rate)**(-term)))
+
+monthly_payment(loan_amount, monthly_rate, term)
+
+
--- lesson_2/morgage/pseudo.md
+++ lesson_2/morgage/pseudo.md
@@ -37,7 +37,8 @@ GET loan amount
 SET variable loan_amount
 
 GET APR
-SET variable APR (if > 1 then divide by 100 to get percentage)
+SET variable APR (if > 1 then divide by 100 to get percentage) 
+while == '0' PRINT "0% interest? That doesn't seem right. Please enter a valid rate:"
 
 GET loan duration in years (confirm no decimals)
 GET loan duration in months (confirm integer between 1 and 12, no      decimals)
@@ -59,5 +60,31 @@ Questions
 ----
 
 what methods can be extrapolated?
+  validation of Y/N response
+  treatment of rate entered as % or not
+  making sure there are only numeric or letter characters
 messages to yaml?
 error messages for data validation issues
+
+
+name = ''
+loop do
+  name = gets.chomp
+  if name.empty? # Add logic for checking for numbers?
+    prompt('valid_name')
+  elsif number?(name)
+    prompt('valid_name')
+  else
+    break
+  end
+end
+
+=begin
+if confirmation.match(/[yY]/)
+  break
+elsif confirmation.match(/[nN]/)
+  puts "Which data point is incorrect? amount, term, or APR?"
+else
+  puts
+end
+=end
