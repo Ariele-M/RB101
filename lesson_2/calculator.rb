6e99b77b509e1a788537f67c1ae0db7dde14536f
Ariele Matthes <ariele0426@gmail.com>
2023-03-03 19:16:13 -0500

    working calculator - 1 language

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -3,76 +3,89 @@
 # perform the operation on the two numbers
 # output the result
 
-def prompt(message)
+require 'yaml'
+MESSAGES = YAML.load_file('calc_config.yml')
+
+LANGUAGE = 'en'
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
+def prompt(key)
+  message = messages(key, LANGUAGE)
+  puts("=> #{message}")
+end
+
+def prompt2(message)
   puts "=> #{message}"
 end
 
 def valid_number?(num)
-  num.to_i.is_a?(Integer)
+  num.to_i.to_s == num
 end
 
-def operation_to_message(op)
-  x = case op
-      when '1'
-        "Adding"
-      when '2'
-        'Subtracting'
-      when '3'
-        'Multiplying'
-      when '4'
-        'Dividing'
-      end
-  prompt "#{x} the two numbers..."
+def number?(num)
+  /\d/.match(num) && /^-?\d*\.?\d*$/.match(num)
 end
 
-prompt "Welcome to Calculator! Enter your name:"
+def operation_to_message(operation)
+  word = case operation
+           when '1'
+             'Adding'
+           when '2'
+             'Subtracting'
+           when '3'
+             'Multiplying'
+           when '4'
+             'Dividing'
+         end
+
+  x = "A random line of code"
+
+  word
+end
+
+prompt('welcome')
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt "Make sure to use a valid name."
+    prompt('valid_name')
   else
     break
   end
 end
 
-prompt("Hi #{name}!")
+prompt2("Hi #{name}!")
 
 loop do # main loop
   number1 = ""
   loop do
-    prompt "What's the first number?"
+    prompt('first_number')
     number1 = gets.chomp
 
-    if valid_number?(number1)
+    if number?(number1)
       break
     else
-      prompt "Hmm.... that doesn't look like a valid number?"
+      prompt('valid_num')
     end
   end
 
   number2 = ""
   loop do
-    prompt "What's the second number?"
+    prompt('second_number')
     number2 = gets.chomp
-    if valid_number?(number2)
+    if number?(number2)
       break
     else
-      prompt "Hmm.... that doesn't look like a valid number?"
+      prompt('valid_num')
     end
   end
 
-  operator_prompt = <<-MSG
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
-
-  prompt(operator_prompt)
+  prompt('operator_prompt')
 
   operator = ""
   loop do
@@ -80,28 +93,28 @@ loop do # main loop
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt "Must choose 1, 2, 3, or 4"
+      prompt('valid_op')
     end
   end
 
-  operation_to_message operator
+  prompt2 "#{operation_to_message(operator)} the two numbers..."
 
   result =  case operator
             when '1'
-              number1.to_i + number2.to_i
+              number1.to_f + number2.to_f
             when '2'
-              number1.to_i - number2.to_i
+              number1.to_f - number2.to_f
             when '3'
-              number1.to_i * number2.to_i
+              number1.to_f * number2.to_f
             when '4'
               number1.to_f / number2.to_f
             end
 
-  prompt("The result is #{result}")
+  prompt2("The result is #{result}")
 
-  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  prompt('go_again')
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thank you for using the calculator. Good bye!")
+prompt('goodbye')
