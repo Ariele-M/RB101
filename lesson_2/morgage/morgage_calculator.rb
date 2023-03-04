dd92ef40db691782225ee01c93657d86411fd894
Ariele Matthes <ariele0426@gmail.com>
2023-03-04 16:18:14 -0500

    morgage calculator total months definition improvement

--- lesson_2/morgage/morgage_calculator.rb
+++ lesson_2/morgage/morgage_calculator.rb
@@ -1,3 +1,4 @@
+require "pry"
 require 'yaml'
 MESSAGES = YAML.load_file('morgage_messages.yml')
 
@@ -14,8 +15,15 @@ def number?(num)
   /\d/.match(num) && /^-?\d*\.?\d*$/.match(num)
 end
 
-def months(years, months)
-  (years * 12) + months
+
+def total_months(years, months)
+  if months.match(/[0-9]/) && months.to_i < 12 #negative numbers
+    p (years.to_i * 12) + months.to_i
+  elsif years == '0' && months.match(/[^0]/)
+    p months.to_i
+  else
+    puts "that is a strange duration format for a loan."
+  end
 end
 
 def monthly_payment(p, j, n)
@@ -25,31 +33,26 @@ end
 prompt('welcome')
 
 loop do
-prompt('amount')
-loan_amount = gets.chomp.to_i
+  prompt('amount')
+  loan_amount = gets.chomp
+  if number?(loan_amount)
+    break
+  else 
+    "That is not a valid dollar amount."
+  end
+end
 
 prompt('years')
-term_years = gets.chomp.to_i
-
+term_years = gets.chomp
 prompt('months')
-term_months = gets.chomp.to_i
+term_months = gets.chomp
+term = total_months(term_years, term_months) 
+
 
 prompt('apr')
 apr = gets.chomp.to_f
 
-puts "Please confirm your loan is for $#{loan_amount} for a term of #{term_years} years and #{term_months} months with an APR of #{apr}% (y/n)"
-confirmation = gets.chomp 
-
-term = months(term_years, term_months)
-
-monthly_rate =  case apr
-                when < 0
-                  apr
-                when > 0
-                  apr/100
-                end
-p monthly_payment = loan_amount * (monthly_rate/(1 - (1 + monthly_rate)**(-term)))
-
-monthly_payment(loan_amount, monthly_rate, term)
-
+puts "Please confirm your loan "
+p confirmation = gets.chomp 
+confirmation
 
