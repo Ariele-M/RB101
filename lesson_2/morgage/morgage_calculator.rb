c9d922d9fcd05e890a531c21f8e353f32ecc30a1
Ariele Matthes <ariele0426@gmail.com>
2023-03-06 20:13:42 -0500

    revision post code review

--- lesson_2/morgage/morgage_calculator.rb
+++ lesson_2/morgage/morgage_calculator.rb
@@ -34,8 +34,41 @@ def percent_format?(apr)
   apr.to_f > 0.18 && apr.to_f <= 18 && apr.match(/^-?\d*\.?\d*\%?$/)
 end
 
-loop do
-  prompt('welcome')
+def prompt_years
+  years = ''
+  loop do
+    prompt('years')
+    years = gets.chomp
+    break if integer?(years)
+    prompt('integer_error')
+  end
+  years
+end
+
+def prompt_months
+  months = ''
+  loop do
+    prompt('months')
+    months = gets.chomp
+    break if integer?(months)
+    prompt('integer_error')
+  end
+  months
+end
+
+def get_years_months
+  years = ''
+  months = ''
+  loop do
+    years = prompt_years
+    months = prompt_months
+    break if valid_term?(years, months)
+    puts "#{years} years and #{months} months is a strange term for a loan."
+  end
+  { years: years, months: months }
+end
+
+def prompt_amount
   amount = ' '
   loop do
     prompt('amount')
@@ -43,53 +76,50 @@ loop do
     break if usd?(amount) && dollar_to_float(amount) != 0
     prompt('amount_error')
   end
+  dollar_to_float(amount)
+end
 
-  amount = dollar_to_float(amount)
+def calculate_monthly_payment(amount, monthly_rate, term)
+  amount * (monthly_rate / (1 - (1 + monthly_rate)**(-term)))
+end
 
-  years = ' '
-  months = ' '
-  loop do
-    loop do
-      prompt('years')
-      years = gets.chomp
-      break if integer?(years)
-      prompt('integer_error')
-    end
+def total_months(years, months)
+  (years.to_i * 12) + months.to_i
+end
 
-    loop do
-      prompt('months')
-      months = gets.chomp
-      break if integer?(months)
-      prompt('integer_error')
-    end
-
-    break if valid_term?(years, months)
-    puts "#{years} years and #{months} months is a strange term for a loan."
-  end
-
-  term = (years.to_i * 12) + months.to_i
-
-  prompt('usury_warning')
-  apr = ' '
-  monthly_rate = ' '
+def prompt_apr
+  apr = ''
   loop do
     prompt('apr')
     apr = gets.chomp
     if rate_format?(apr)
-      monthly_rate = apr.to_f / 12
       apr = apr.to_f * 100
       break
     elsif percent_format?(apr)
-      monthly_rate = apr.to_f / 1200
       apr = apr.to_f
       break
     else
       prompt('rate_error')
     end
   end
+  apr
+end
 
-  monthly_payment = amount *
-                    (monthly_rate / (1 - (1 + monthly_rate)**(-term)))
+loop do
+  prompt('welcome')
+
+  amount = prompt_amount
+
+  years_months = get_years_months
+  years = years_months[:years]
+  months = years_months[:months]
+  term = total_months(years, months)
+
+  prompt('usury_warning')
+  apr = prompt_apr
+  monthly_rate = apr.to_f / 1200
+
+  monthly_payment = calculate_monthly_payment(amount, monthly_rate, term)
 
   result_message = <<-MSG
     Your monthly payment is $#{format('%.2f', monthly_payment)} 
