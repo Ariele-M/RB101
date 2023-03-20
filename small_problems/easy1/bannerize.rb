b13f1747e8afffc5bb17869cc08377d31b98a193
Ariele Matthes <ariele0426@gmail.com>
2023-03-20 13:44:17 -0400

    bannerize attempt 1

--- small_problems/easy1/bannerize.rb
+++ small_problems/easy1/bannerize.rb
@@ -1,6 +1,12 @@
-# def print_in_box(str)
-#   x = str.length
-#   x.times(print "-")
-# end
+def print_in_box(str)
+  x = str.size
+  puts "+-" + ("-" * x) + "-+"
+  puts "| " + (" " * x ) + " |"
+  puts "| " + str + " |"
+  puts "| " + (" " * x) + " |"
+  puts "+-" + ("-" * x) + ("-+")
+end
 
-# print_in_box('To boldly go where no one has gone before.')
+print_in_box('To boldly go where no one has gone before.')
+
+print_in_box('')
