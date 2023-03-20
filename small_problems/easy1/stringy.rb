b5c77145748dff9c2c78ed7e6b1ec8d37bcbe126
Ariele Matthes <ariele0426@gmail.com>
2023-03-20 13:20:17 -0400

    Even tests passed  for stringy

--- small_problems/easy1/stringy.rb
+++ small_problems/easy1/stringy.rb
@@ -10,14 +10,18 @@ end
 
 def stringy(number)
   string = ''
-    number.times do
-       string += '10'
+  if number.even?
+    string = "10" * (number / 2)
+  elsif number == 1
+    string = "1"
+  else
+    string = ("10" * ((number -  2) / 2)) + "1"
   end
-  string
 end
 
 
+
 puts stringy(6) == '101010'
-#puts stringy(9) == '101010101'
+puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
-#puts stringy(7) == '1010101'
+puts stringy(7) == '1010101'
